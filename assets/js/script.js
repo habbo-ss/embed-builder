cd3a1a00012e962cc2ab79d0be31dafb0becf1ae
habbo-ss <habbohabbo24alt@gmail.com>
2025-04-06 13:07:53 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -877,7 +877,6 @@ addEventListener('DOMContentLoaded', () => {
                             console.log(base64String);
                             fetch('https://script.google.com/macros/s/AKfycbzSL1mnKfaFy0EPPe9Ev3K3a7H_cirYCUaaTdXZiGPqznzs-SA3vgRF4W1Q8XaJY-dz/exec', {
                                 method: "POST",
-                                mode: "no-cors",
                                 headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
                                 body: JSON.stringify({ image: base64String })
                                 })
