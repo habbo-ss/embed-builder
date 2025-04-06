7061f982ef66725c0ba7975efe9f511e5f5d5a89
habbo-ss <habbohabbo24alt@gmail.com>
2025-04-06 14:18:30 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ window.inIframe ??= top !== self;
 
 window.addEventListener("message", function(event) {
     if (event.data === "requestData") {
-        event.source.postMessage("Hello from Iframe!", event.origin);
+        event.source.postMessage(window.json, event.origin);
     }
 });
 
